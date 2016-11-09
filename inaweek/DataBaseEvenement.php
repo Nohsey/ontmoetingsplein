e6286c6ffff42eee0ad317faded5c1e8661e9d73
mipi70 <kyleritchi123@gmail.com>
2016-11-09 09:39:50 +0100

    backend agenda

--- inaweek/DataBaseEvenement.php
+++ inaweek/DataBaseEvenement.php
@@ -18,7 +18,7 @@ public function CreateEvent($ETitel ,$EInhoud ,$AdminId)
 public function GetEventNameOnDate($date)
 {
     $connection = $_SESSION['Connection'];
-    $sql = "SELECT ETitel FROM `Evenementen` WHERE Datum='$date'";
+    $sql = "SELECT ETitel FROM `Evenementen` WHERE Datum='$date' order by time ASC";
 	  $result = mysqli_query($connection, $sql);
     $titles = null;
     while($row = mysqli_fetch_array($result))
@@ -28,5 +28,25 @@ public function GetEventNameOnDate($date)
 
 
     return $titles;
+}
+
+public function GetEventBTime($title)
+{
+    $connection = $_SESSION['Connection'];
+    $sql = "SELECT BeginTijd FROM Evenementen where ETitel=$title";
+    $result = mysqli_query($connection, $sql);
+    $BTijd= mysqli_fetch_array($result);
+
+    return $BTijd[0];
+}
+
+public function GetEventETime($title)
+{
+    $connection = $_SESSION['Connection'];
+    $sql = "SELECT EindTijd FROM Evenementen where ETitel=$title";
+    $result = mysqli_query($connection, $sql);
+    $ETijd= mysqli_fetch_array($result);
+
+    return $ETijd[0];
 }
 }
