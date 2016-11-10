ac581bf178c2b9a42980937830fa49733e870a1c
mipi70 <kyleritchi123@gmail.com>
2016-11-10 13:16:39 +0100

    aanvraag pagina

--- inaweek/DataBaseEvenement.php
+++ inaweek/DataBaseEvenement.php
@@ -32,7 +32,7 @@ public function GetEventNameOnDate($date)
 public function GetEventName()
 {
     $connection = $_SESSION['Connection'];
-    $sql = "SELECT ETitel FROM `Evenementen` WHERE `EindTijd` >= NOW() AND `Datum` >= CURDATE() ORDER BY Datum";
+    $sql = "SELECT ETitel FROM `Evenementen` WHERE `EindTijd` >= NOW() AND `Datum` >= CURDATE() ORDER BY Datum, BeginTijd";
 	  $result = mysqli_query($connection, $sql);
     $titles = null;
     while($row = mysqli_fetch_array($result))
@@ -47,7 +47,7 @@ public function GetEventName()
 public function GetEventinhoud($title)
 {
     $connection = $_SESSION['Connection'];
-    $sql = "SELECT EInhoud FROM `Evenementen` WHERE `EindTijd` >= NOW() AND `Datum` >= CURDATE() AND `ETitel`='$title'  ORDER BY Datum";
+    $sql = "SELECT EInhoud FROM `Evenementen` WHERE `EindTijd` >= NOW() AND `Datum` >= CURDATE() AND `ETitel`='$title'  ORDER BY Datum, BeginTijd";
 	  $result = mysqli_query($connection, $sql);
     $EInhoud = null;
     while($row = mysqli_fetch_array($result))
@@ -57,36 +57,6 @@ public function GetEventinhoud($title)
 
 
     return $EInhoud[0];
-}
-
-public function GetEventTimeStart($title)
-{
-    $connection = $_SESSION['Connection'];
-    $sql = "SELECT BeginTijd FROM `Evenementen` WHERE `EindTijd` >= NOW() AND `Datum` >= CURDATE() AND `ETitel`='$title'  ORDER BY Datum";
-	  $result = mysqli_query($connection, $sql);
-    $Btijd = null;
-    while($row = mysqli_fetch_array($result))
-    {
-        $Btijd = $row;
-    }
-
-
-    return $Btijd[0];
-}
-
-public function GetEventTimeEind($title)
-{
-    $connection = $_SESSION['Connection'];
-    $sql = "SELECT EindTijd FROM `Evenementen` WHERE `EindTijd` >= NOW() AND `Datum` >= CURDATE() AND `ETitel`='$title'  ORDER BY Datum";
-	  $result = mysqli_query($connection, $sql);
-    $Etijd = null;
-    while($row = mysqli_fetch_array($result))
-    {
-        $Etijd = $row;
-    }
-
-
-    return $Etijd[0];
 }
 
 public function CountEvent()
@@ -145,4 +115,95 @@ public function GetEventData($title)
 
     return $fulldata;
 }
+
+
+
+
+public function GetEventName1()
+{
+    $connection = $_SESSION['Connection'];
+    $sql = "SELECT aeName FROM `AanvraagEvenement` WHERE `aeDate` >= CURDATE() ORDER BY aeDate, aeBeginTijd";
+	  $result = mysqli_query($connection, $sql);
+    $titles = null;
+    while($row = mysqli_fetch_array($result))
+    {
+        $titles[] = $row;
+    }
+
+
+    return $titles;
+}
+
+public function GetEventinhoud1($title)
+{
+    $connection = $_SESSION['Connection'];
+    $sql = "SELECT aeInhoud FROM `AanvraagEvenement` WHERE `aeDate` >= CURDATE() AND `aeName`='$title'  ORDER BY aeDate, aeBeginTijd";
+	  $result = mysqli_query($connection, $sql);
+    $EInhoud = null;
+    while($row = mysqli_fetch_array($result))
+    {
+        $EInhoud = $row;
+    }
+
+
+    return $EInhoud[0];
+}
+
+public function CountEvent1()
+{
+    $connection = $_SESSION['Connection'];
+    $sql = "SELECT COUNT(aeName) FROM `AanvraagEvenement` WHERE `aeDate` >= CURDATE()";
+	  $result = mysqli_query($connection, $sql);
+
+
+
+    $count = mysqli_fetch_array($result);
+
+
+    return $count[0];
+}
+
+public function GetEventBTime1($title)
+{
+    $connection = $_SESSION['Connection'];
+    $sql = "SELECT aeBeginTijd FROM AanvraagEvenement where aeName='$title'";
+    $result = mysqli_query($connection, $sql);
+    $BTijd= mysqli_fetch_array($result);
+
+		$pieces = explode(":", $BTijd[0]);
+
+		$BTijddone = $pieces[0].":".$pieces[1];
+
+
+    return $BTijddone;
+}
+
+public function GetEventETime1($title)
+{
+    $connection = $_SESSION['Connection'];
+    $sql = "SELECT aeEindTijd FROM AanvraagEvenement where aeName='$title'";
+    $result = mysqli_query($connection, $sql);
+    $ETijd= mysqli_fetch_array($result);
+
+		$pieces = explode(":", $ETijd[0]);
+
+		$ETijddone = $pieces[0].":".$pieces[1];
+
+
+    return $ETijddone;
+}
+public function GetEventData1($title)
+{
+    $connection = $_SESSION['Connection'];
+    $sql = "SELECT aeDate FROM AanvraagEvenement where aeName='$title'";
+    $result = mysqli_query($connection, $sql);
+    $datum= mysqli_fetch_array($result);
+
+		$pieces = explode("-", $datum[0]);
+
+		$fulldata = $pieces[2]."-".$pieces[1]."-".$pieces[0];
+
+    return $fulldata;
+}
+
 }
